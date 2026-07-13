b88a5deb6a3449ed7a656837eea5eacd841237de
Juan Lagoria <lagoria79@gmail.com>
2026-07-13 19:35:26 -0300

    fix: return 400 for malformed JSON request bodies
    - Catch SyntaxError from Fastify JSON parser in error handler
    - Return VALIDATION_ERROR instead of SERVER_ERROR

--- linear-clone-backend-js/src/shared/errors/error-handler.ts
+++ linear-clone-backend-js/src/shared/errors/error-handler.ts
@@ -1,9 +1,13 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
-import { BaseError, InternalError } from './index';
+import { BaseError, InternalError, ValidationError } from './index';
 
 export function errorHandler(error: Error, request: FastifyRequest, reply: FastifyReply) {
   if (error instanceof BaseError) {
     return reply.status(error.statusCode).send(error.toJSON());
+  }
+
+  if (error instanceof SyntaxError && 'body' in error) {
+    return reply.status(400).send(new ValidationError('Malformed JSON in request body').toJSON());
   }
 
   request.log.error(error);
